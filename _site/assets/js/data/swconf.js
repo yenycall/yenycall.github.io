6e83288e4895d361f0bcd2524e3ffa35812a3ba9
yenycall <yenycall7@naver.com>
2024-10-15 21:22:56 +0900

    .

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728994905',resources: [
+    cacheName: 'chirpy-1728994970',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
