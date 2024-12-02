9732c1eecf4385939b4fcf60e9855c59b355d8b4
yenycall <yenycall7@naver.com>
2024-12-02 14:12:53 +0900

    수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732147447',resources: [
+    cacheName: 'chirpy-1732711937',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
